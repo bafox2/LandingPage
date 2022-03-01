d17f8d075e5ba32aa3301ed48d45d0de00f54539
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-02-28 20:52:39 -0500

    maps

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,7 @@
+import { Loader, LoaderOptions } from 'google-maps';
+
+
+
 let contact = (() => {
     let contactdiv = document.createElement('div')
     contactdiv.classList.add("contact");
@@ -5,8 +9,20 @@ let contact = (() => {
     phone.innerText = "703-555-5555"
     let address = document.createElement('p')
     address.innerText = "408 W St Clair Ave, Cleveland, OH 44113"
+    let mapwrapper = document.createElement('#map')
+    const options: LoaderOptions = { /* todo */ };
+    const loader = new Loader('AIzaSyCO_zgzQPLw8QumirmZWjHwgRDFrdIMooM', options);
+
+    const google = await loader.load();
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+
+
     contactdiv.appendChild(phone)
     contactdiv.appendChild(address)
+    contactdiv.appendChild(mapwrapper)
     document.querySelector('#content').appendChild(footer)
 })();
-export default footer;
+export default contact;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Loader } = require('google-maps');
 
 module.exports = {
     entry: './src/index.js',
