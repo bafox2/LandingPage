523470a1f62ae45f14391f329eeb4c0661bc5796
benjamin fox <benjaminalexanderfox11@gmail.com>
2022-03-11 18:50:23 -0500

    touchup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './styles.css';
-import getMenu from './menu.js';
 import footer from './footer.js';
 import header from './header.js';
-import mapContent from './contact.js';
-import aboutContent from './about.js';
 import { init, buttonAssignment } from './navigation.js';
 
 header()
